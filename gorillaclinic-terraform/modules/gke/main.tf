77d87a79a76ae69457e9084d2f5867fea1848d5f
Shabadh Ellath <shabadhe@ymail.com>
2025-06-08 15:53:55 +0200

    updated wip

--- gorillaclinic-terraform/modules/gke/main.tf
+++ gorillaclinic-terraform/modules/gke/main.tf
@@ -17,7 +17,7 @@ resource "google_container_cluster" "primary" {
   }
 
   workload_identity_config {
-    workload_pool = "${data.google_project.live.number}.svc.id.goog"
+    workload_pool = "${var.project_id}.svc.id.goog"
   }
 
   vertical_pod_autoscaling {
