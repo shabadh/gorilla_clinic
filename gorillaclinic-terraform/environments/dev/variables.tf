cdc59422ec6cfdadcc865e22f52e73db2451c1b4
Shabadh Ellath <shabadhe@ymail.com>
2025-06-08 13:49:49 +0200

    var corrected

--- gorillaclinic-terraform/environments/dev/variables.tf
+++ gorillaclinic-terraform/environments/dev/variables.tf
@@ -2,7 +2,9 @@ variable "project_id" {}
 variable "vpc_name" {}
 variable "subnets" {}
 # variable "cluster_name" {}
-variable "regions" {}
+variable "regions" {
+    type = list(string)
+}
 # variable "pod_range" {}
 # variable "service_range" {}
 # variable "region" {}
