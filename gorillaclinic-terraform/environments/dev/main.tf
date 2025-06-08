bbcb6cbf80499bb93266b44d789610690b9b6748
Shabadh Ellath <shabadhe@ymail.com>
2025-06-08 13:40:53 +0200

    commented the gke

--- gorillaclinic-terraform/environments/dev/main.tf
+++ gorillaclinic-terraform/environments/dev/main.tf
@@ -17,13 +17,13 @@ module "vpc" {
   regions    = var.region
 }
 
-module "gke_autopilot" {
-  source        = "../../modules/gke_autopilot"
-  project_id    = var.project_id
-  cluster_name  = var.cluster_name
-  region        = var.region
-  vpc_id        = module.vpc.vpc_id
-  subnet_id     = module.vpc.subnet_ids["gc-nl-subnet1-dev"]
-  pod_range     = var.pod_range
-  service_range = var.service_range
-}
+# module "gke_autopilot" {
+#   source        = "../../modules/gke_autopilot"
+#   project_id    = var.project_id
+#   cluster_name  = var.cluster_name
+#   region        = var.region
+#   vpc_id        = module.vpc.vpc_id
+#   subnet_id     = module.vpc.subnet_ids["gc-nl-subnet1-dev"]
+#   pod_range     = var.pod_range
+#   service_range = var.service_range
+# }
