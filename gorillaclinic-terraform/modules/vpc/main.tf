5366e232ec0bbdf4bf821e6a451487c434321d20
Shabadh Ellath <shabadh.ellath@rabobank.nl>
2025-06-09 14:10:18 +0200

    no changes

--- gorillaclinic-terraform/modules/vpc/main.tf
+++ gorillaclinic-terraform/modules/vpc/main.tf
@@ -23,28 +23,6 @@ resource "google_compute_subnetwork" "subnets" {
   }
 }
 
-# resource "google_compute_global_address" "private_ip_address" {
-#   provider = google-beta
-
-#   name          = "private-ip-address"
-#   purpose       = "VPC_PEERING"
-#   address_type  = "INTERNAL"
-#   prefix_length = 16
-#   network       = google_compute_network.private_network.id
-# }
-
-# resource "google_service_networking_connection" "private_vpc_connection" {
-#   provider = google-beta
-
-#   network                 = google_compute_network.private_network.id
-#   service                 = "servicenetworking.googleapis.com"
-#   reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
-# }
-
-# resource "random_id" "db_name_suffix" {
-#   byte_length = 4
-# }
-
 resource "google_compute_global_address" "private_service_range" {
   name          = "google-managed-services-${var.vpc_name}"
   purpose       = "VPC_PEERING"
